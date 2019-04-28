f7c6d3ce6c200aa53c033af72123ab08c8f87483
R4D4R125 <48413277+R4D4R125@users.noreply.github.com>
2019-04-28 22:55:00 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// function toggleSidebar(params) {
-//   document.getElementById("navbar").classList.toggle("container_items");
-// }
-
-// $(document).ready(function() {
-//   $(".menu_btn").click(function() {
-//     $(".item_2").animate({ left: "250px" });
-//     width: "toggle";
-//   });
-// });
-
 $(document).ready(function animation() {
   $(".menu_btn").click(function() {
     $(".container_2").toggleClass("active");
